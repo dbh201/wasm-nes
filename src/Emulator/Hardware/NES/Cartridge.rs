4b9ae8018d325232ac64ec79c4aa279425f604d5
dbh201 <47301809+dbh201@users.noreply.github.com>
2023-04-19 13:49:51 -0300

    Cartridge: support header version test

--- src/Emulator/Hardware/NES/Cartridge.rs
+++ src/Emulator/Hardware/NES/Cartridge.rs
@@ -31,6 +31,13 @@ struct NESHeader {
     padding_15: u8,
 }
 
+#[derive(PartialEq)]
+enum NESHeaderVersion {
+    Archaic,
+    INES,
+    NES2_0,
+}
+
 impl NESHeader {
     pub fn new(data: &Vec<u8>) -> Result<NESHeader,String> {
         let magicnum = [ 0x4E, 0x45, 0x53, 0x1A ];
@@ -55,13 +62,43 @@ impl NESHeader {
         })
     }
     pub fn prgsize(&self) -> usize {
-        return (self.prg_rom_size as usize)<<14
+        if self.header_version() == NESHeaderVersion::Archaic {
+            return (self.prg_rom_size as usize)<<14
+        } else {
+            if self.flags_9 & 0x0F < 0x0F {
+                return (((self.flags_9 as usize & 0x0F) << 8) + self.prg_rom_size as usize)<<14
+            } else {
+                return (2>>((self.prg_rom_size as usize)&0xFC)) * ((self.prg_rom_size&0x03)+1) as usize
+            }
+        }
     }
     pub fn chrsize(&self) -> usize {
-        return (self.chr_rom_size as usize)<<13
+        if self.header_version() == NESHeaderVersion::Archaic {
+            return (self.chr_rom_size as usize)<<13
+        } else {
+            if self.flags_9 & 0xF0 < 0xF0 {
+                return (((self.flags_9 & 0xF0) << 4) as usize + self.chr_rom_size as usize)<<13
+            } else {
+                return (2>>((self.chr_rom_size as usize)&0xFC)) * ((self.chr_rom_size&0x03)+1) as usize
+            }
+        }
     }
-    pub fn mapper(&self) -> u8 {
-        return (self.flags_6 >> 4) + (self.flags_7 & 0xF0)
+    pub fn header_version(&self) -> NESHeaderVersion {
+        if self.flags_7 & 0x0C == 0x04 {
+            return NESHeaderVersion::Archaic;
+        }
+        if self.flags_7 & 0x0C == 0x00 {
+            if self.padding_12 == 0 && self.padding_13 == 0 && self.padding_14 == 0 && self.padding_15 == 0 {
+                return NESHeaderVersion::INES;
+            }
+        }
+        if self.flags_7 & 0x0C == 0x08 && ((self.flags_9 as usize)<<8) <= self.prgsize() + self.chrsize() + 0x10 {
+            return NESHeaderVersion::NES2_0;
+        }
+        return NESHeaderVersion::Archaic;
+    }
+    pub fn mapper(&self) -> u16 {
+            return ((self.flags_6 >> 4) + (self.flags_7 & 0xF0)) as u16
     }
     pub fn prg_rom_start(&self) -> usize {
         return 0x10;
@@ -138,6 +175,17 @@ impl<'a> Cartridge<'a> {
             chrrom.bulk_set(0,data[header.chr_rom_start()..=header.chr_rom_end()].to_vec())?;
             chrrom.add_addr_range(0, 0x1FFF)?;
             self.ppu_bus.borrow_mut().register_AddressNode(chrrom)?;
+            console_log!("Testing CHR ROM...");
+            for x in 0..=0x1FFF {
+                let busval = self.ppu_bus.borrow_mut().get(x)?;
+                let romval = data[header.chr_rom_start() + x as usize];
+                if busval != romval {
+                    return Err(format!("{} != {}", busval, romval))
+                }
+                if busval == 0 {
+
+                }
+            }
 
             console_log!("Mapping VRAM...");
             let mut vram = AddressNode::new(format!("[{}].VRAM_MAP",self.name).to_owned());
