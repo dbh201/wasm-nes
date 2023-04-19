c641e26dc614c03aee5bf27c99cabba20f640721
dbh201 <47301809+dbh201@users.noreply.github.com>
2023-04-19 13:29:37 -0300

    simulated joystick input

--- src/Emulator/Hardware/NES/APUJoystick.rs
+++ src/Emulator/Hardware/NES/APUJoystick.rs
@@ -2,18 +2,33 @@ use crate::Emulator::Common::AddressNode::AddressObject;
 
 pub struct APUJoystick {
     //TODO: sound support
-    status: u8
+    status: u8,
+    joy1: u8,
+    joy2: u8,
+    out: u8,
 }
 impl APUJoystick {
     pub fn new() -> Result<APUJoystick,String> {
-        Ok(APUJoystick { status: 0 })
+        Ok(APUJoystick { status: 0, joy1: 0, joy2: 0, out: 0 })
     }
 }
 impl AddressObject for APUJoystick {
     fn get(&mut self, addr: u16) -> Result<u8, String> {
+        if addr == 0x0016 {
+            // Read port 1
+            return Ok(0b00011001)
+        }
+        if addr == 0x0017 {
+            // Read port 2
+            return Ok(0b00011001)
+        }
         Ok(0)
     }
     fn set(&mut self, addr: u16, val: u8) -> Result<(), String> {
+        if addr == 0x0016 {
+            // Retrieve joystick input
+            self.out = val;
+        }
         Ok(())
     }
     fn len(&self) -> usize {
